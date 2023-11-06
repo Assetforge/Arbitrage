72256f2f7f39eff3662374a10b40f896fde9cee5
Assetforge <129163554+Assetforge@users.noreply.github.com>
2023-11-06 13:15:43 +0100

    Update Arbitrage.py
    Optimized functions

--- Arbitrage.py
+++ Arbitrage.py
@@ -1,157 +1,89 @@
 import requests
 import time
 import matplotlib.pyplot as plt
-import numpy as np
-import math
 from binance.client import Client
 from binance.enums import *
 from pybit.unified_trading import HTTP
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 from decimal import Decimal, ROUND_DOWN
+# import numpy as np
+# import math
 
 # Initialize your API Keys and Secrets for Binance and Bybit
-BINANCE_API_KEY = 'FAwoao710g15n9CYv3FXiaclCpGN59V8XK8ipHIyi6wykO6JrSmPa0optsmH02W3'
-BINANCE_API_SECRET = 'AiP11eVHbtaL73JdlGcNIsO4yaSHMSl8yhCrbRxg9Bb6qPaTPGl8og5dQMn6KMvo'
-BYBIT_API_KEY = '7JeEcSvWEW7c2VGBdI'
-BYBIT_API_SECRET = 'zobIPQ0YIOzkPwiDdM8A8V0P1ZmrCdAQaHZJ'
+BINANCE_API_KEY = ''
+BINANCE_API_SECRET = ''
+BYBIT_API_KEY = ''
+BYBIT_API_SECRET = ''
 
 client = Client(BINANCE_API_KEY, BINANCE_API_SECRET)
+bybit_session = HTTP(testnet=False, api_key=BYBIT_API_KEY, api_secret=BYBIT_API_SECRET)
+binance_session = requests.Session()
 
-# Function to get current price from Binance, symbol is a string
 
-def get_binance_price(symbol):
+def get_bi_price(symbol):
     url = f'https://api.binance.com/api/v3/ticker/bookTicker?symbol={symbol}'
-
-    response = requests.get(url)
-    response.raise_for_status()  # Check if the request was successful
-    data = response.json()
-
-    # Extracting the bid and ask prices
-    bid_price = float(data['bidPrice'])
-    ask_price = float(data['askPrice'])
-
-    # Calculating the spread
-    spread = ask_price - bid_price
-
-    return bid_price, ask_price, spread
+    data = binance_session.get(url).json()
+    return float(data['bidPrice']), float(data['askPrice'])
 
 
-# Function to get current price from Bybit, SYMBUSDT is a string
-
-def get_bybit_price(symbol):
-    r = requests.get(f'https://api.bybit.com/v2/public/tickers?symbol={symbol}')
-    r.raise_for_status()  # Raise an HTTPError for bad responses
-    data = r.json()
-
-    # Accessing the 'bid_price' and 'ask_price' keys from the first dictionary in the list
-    bid_price = float(data['result'][0]['bid_price'])
-    ask_price = float(data['result'][0]['ask_price'])
-
-    # Calculating the spread
-    spread = ask_price - bid_price
-
-    return bid_price, ask_price, spread
+def get_by_price(symbol):
+    url = f'https://api.bybit.com/v2/public/tickers?symbol={symbol}'
+    data = requests.get(url).json()['result'][0]
+    return float(data['bid_price']), float(data['ask_price'])
 
 
 def is_ao(symbol, seuil):  # Boolean function which returns true if there exists an arbitrage opportunity. Seuil between 0 and 1
+    bi_bid_price, bi_ask_price = get_bi_price(symbol)
+    by_bid_price, by_ask_price = get_by_price(symbol)
+    return (by_ask_price - bi_bid_price) / bi_bid_price > seuil
 
-    bi_bid_price, bi_ask_price, bi_spread = get_binance_price(symbol)
-    by_bid_price, by_ask_price, by_spread = get_bybit_price(symbol)
-
-    if (by_ask_price - bi_bid_price) / bi_bid_price > seuil:
-        return True
-    #elif  (bi_ask_price - by_bid_price) / by_bid_price > seuil: #cannot short on binance
-        #return True
-    else:
-        return False
 
 def is_to_close(symbol, seuil):  # Boolean function which returns true if the price difference is negligeable to the Seuil between 0 and 1
-
-    bi_bid_price, bi_ask_price, bi_spread = get_binance_price(symbol)
-    by_bid_price, by_ask_price, by_spread = get_bybit_price(symbol)
-
-    if (by_ask_price - bi_bid_price) / bi_bid_price < seuil:
-        return True
-    else:
-        return False
+    bi_bid_price, bi_ask_price = get_bi_price(symbol)
+    by_bid_price, by_ask_price = get_by_price(symbol)
+    return (by_ask_price - bi_bid_price) / bi_bid_price < seuil
 
 
-def binance_order(Side, quantity, trading_pair) : # Side : either SIDE_SELL or SIDE_BUY, attention à la quantité minimum. EX : binance_order(SIDE_BUY, 0.03, "BNBUSDT")
-
-    if Side == SIDE_BUY:
-        print(f"Opening Binance position: BUY {quantity} {trading_pair}\n")
-    elif Side == SIDE_SELL and binance_position_open:
-        print(f"Closing Binance position: SELL {quantity} {trading_pair}\n")
+def bi_order(side, quantity, trading_pair):
+    print(f"{'Opening' if side == SIDE_BUY else 'Closing'} Binance position: {side} {quantity} {trading_pair}\n")
+    return client.create_order(symbol=trading_pair, side=side, type=ORDER_TYPE_MARKET, quantity=quantity)
 
 
-    order_type = ORDER_TYPE_MARKET
-
-    # Place the order
-    order = client.create_order(
-        symbol=trading_pair,
-        side=Side,
-        type=order_type,
-        quantity=quantity
-    )
-    return print(order)
+def by_order(side, quantity, trading_pair):
+    print(f"{'Opening' if side == 'Sell' else 'Closing'} Bybit position: {side} {quantity} {trading_pair}\n")
+    return bybit_session.place_order(category="linear", symbol=trading_pair, side=side, orderType="Market", qty=quantity, timeInForce="FillOrKill", isLeverage=0)
 
 
-#Function to place an order on Bybit
-
-def bybit_order(Side, quantity, trading_pair) : # Side : "Buy" or "Sell", quantity in bracket  trading_pair "SOLUSDT", bybit_order("Buy", "0.1", "SOLUSDT")
-
-    if Side == "Sell":
-        print(f"Opening Bybit position: SELL {quantity} {trading_pair}\n")
-    elif Side == "Buy" and bybit_position_open:
-        print(f"Closing Bybit position: BUY {quantity} {trading_pair}\n")
-
-    session = HTTP(
-        testnet=False,
-        api_key = BYBIT_API_KEY,
-        api_secret = BYBIT_API_SECRET,
-    )
-
-    order = session.place_order(
-        category="linear",
-        symbol=trading_pair,
-        side=Side,
-        orderType="Market",
-        qty=quantity,
-        timeInForce="FillOrKill",
-        isLeverage=0,
-        )
-    return print(order)
-
-# Idée : vérification si les deux positions ont bien été ouvertes pour continuer.
-#quand et comment fermer la position ?
+def get_asset_balance(asset):#for BINANCE !
+    return client.get_asset_balance(asset=asset)
 
 
-def main(trading_pair):
+def main(trading_pair, quantity, seuil_a, seuil_c):
 
-    bybit_position_open = False
-    binance_position_open = False
-    while True:  # Infinite loop to keep the script running
+    positions_open = False
+    symbol = trading_pair.replace("USDT", "")
+
+    while True:
         try:
-            if is_ao(trading_pair, 0.005) and not binance_position_open :
+            if is_ao(trading_pair, seuil_a) and not positions_open :
                 print("Arbitrage opportunity detected. \n")
-                binance_order(SIDE_BUY, 0.5, trading_pair)
-                bybit_order("Sell", "0.5", trading_pair)#add str(quantity) as a parameter in the function
-                binance_position_open = True
+                bi_order(SIDE_BUY, quantity, trading_pair)
+                by_order("Sell", str(quantity), trading_pair)
+                positions_open = True
 
-            if is_to_close(trading_pair, 0.001) and binance_position_open: # and bybit_position_open
-                asset_balance = get_asset_balance("SOL")['free']
-                asset_balance = Decimal(asset_balance)
+            if is_to_close(trading_pair, seuil_c) and positions_open: # and bybit_position_open
+                asset_balance = Decimal(get_asset_balance(symbol)['free'])
                 asset_balance_adj = asset_balance.quantize(Decimal('0.01'), rounding=ROUND_DOWN)# A adapter à la crypto arbitrée
 
-                binance_order(SIDE_SELL, asset_balance_adj, trading_pair)
-                bybit_order("Buy", str(asset_balance_adj), trading_pair)
-                binance_position_open = False
+                bi_order(SIDE_SELL, asset_balance_adj, trading_pair)
+                by_order("Buy", str(asset_balance_adj), trading_pair)
+                positions_open = False
                 print("position closed. \n")
 
-            if binance_position_open :
+            if positions_open :
                 print("Waiting to close positions \n")
 
-            if not is_ao(trading_pair, 0.01) and not binance_position_open:
+            if not is_ao(trading_pair, seuil_a) and not positions_open:
                 print("No arbitrage opportunity at the moment.\n")
 
 
@@ -162,37 +94,5 @@ def main(trading_pair):
         time.sleep(2)  # Wait for 1 second before checking again
 
 
-
-def get_asset_balance(asset):#for BINANCE !
-    # Get the balance of a specific asset
-    account_balance = client.get_asset_balance(asset=asset)
-    return account_balance
-
-# Call the function to display all non-zero spot balances
-def close_position_binance(asset, trading_pair):
-    try:
-        # Get the current balance of the asset you want to sell
-        asset_balance = get_asset_balance(asset)['free']
-        asset_balance = Decimal(asset_balance)
-        asset_balance_adj = asset_balance.quantize(Decimal('0.01'), rounding=ROUND_DOWN)
-
-        if asset_balance > 0:
-            # If you have a balance, place a MARKET sell order
-            order = client.order_market_sell(
-                symbol=trading_pair,
-                quantity=asset_balance_adj
-            )
-            return order
-        else:
-            print(f"You don't have any {asset} to sell.")
-            return None
-
-    except BinanceAPIException as e:
-        print(f"Binance API Exception: {e}")
-        return None
-    except BinanceOrderException as e:
-        print(f"Binance Order Exception: {e}")
-        return None
-
-
+#main("TRBUSDT", 0.25, 0.007, 0.0001)
 
